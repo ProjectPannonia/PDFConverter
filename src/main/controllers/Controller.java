3ad847d941a5e068799f231249cbf681758a70b6
Letenyei Ádám <letenyeidam89@gmail.com>
2020-11-08 20:40:19 +0100

    Buttons working

--- src/main/controllers/Controller.java
+++ src/main/controllers/Controller.java
@@ -4,10 +4,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
-import main.service.CheckConversionSuccesfulness;
 import main.service.PDFtoImage;
 
 import java.io.File;
@@ -16,23 +14,20 @@ import java.util.List;
 public class Controller {
 
     @FXML
-    TextField P1PathToPDFtf, P1PathToResulttf;
+    Button P1ConvertButton, P1MultipleFileChooser, P1ChooseDestinationFolderBtn;
 
     @FXML
-    Button P1ConvertButton, P1MultipleFileChooser;
-
-    @FXML
-    Label P1ResultLabel;
+    Label P1ResultLabel, P1SelectedPDFFile, P1ChooseDestinationFolderLb;
 
     @FXML
     public void p1Convert(ActionEvent e) {
-        String sourcePath = P1PathToPDFtf.getText();
-        String destinationPath = P1PathToResulttf.getText();
-        String response = "Working!";
-        boolean ran = PDFtoImage.convertToImages(sourcePath,"JPG",destinationPath);
-
-        while (!ran) {
-            response = CheckConversionSuccesfulness.isWritingSuccesfully(destinationPath);
+        String pdfChooserLabel = P1SelectedPDFFile.getText();
+        String destinationLabel = P1ChooseDestinationFolderLb.getText() + "\\";
+        if (pdfChooserLabel != null && pdfChooserLabel != "" && destinationLabel != null && destinationLabel != "") {
+            System.out.println("kezdem");
+            P1ResultLabel.setText("Working");
+            System.out.println(pdfChooserLabel + ", " + destinationLabel);
+            PDFtoImage.convertToImages(pdfChooserLabel,"JPG",destinationLabel);
         }
     }
     @FXML
@@ -42,6 +37,24 @@ public class Controller {
         List<File> f = fc.showOpenMultipleDialog(null);
         for(File file : f) {
             System.out.println(file.getAbsolutePath());
+        }
+    }
+    @FXML
+    public void singlePDFChooser(ActionEvent e) {
+        FileChooser fc = new FileChooser();
+        fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("PDF Files","*.pdf"));
+        File f = fc.showOpenDialog(null);
+        if(f != null) {
+            P1SelectedPDFFile.setText(f.getAbsolutePath());
+        }
+    }
+    @FXML
+    public void singleDirectoryChooser(ActionEvent e) {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        File directory = directoryChooser.showDialog(null);
+        if (directory != null) {
+            String destinationFolderPath = directory.getAbsolutePath();
+            P1ChooseDestinationFolderLb.setText(destinationFolderPath);
         }
     }
 }
