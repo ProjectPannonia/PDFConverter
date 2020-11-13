7d21e148b083a947c1ed9c467ed7e9865c1a5010
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2020-11-13 14:02:56 +0100

    Create GetTextFromPDF.java

---
+++ src/main/service/modify/GetTextFromPDF.java
@@ -0,0 +1,19 @@
+package main.service.modify;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import java.io.IOException;
+
+public class GetTextFromPDF {
+    public static String getText(PDDocument document) {
+        String text = null;
+        try {
+            text = new PDFTextStripper().getText(document);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return text;
+    }
+}
