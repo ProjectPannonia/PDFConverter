2df317da5912fe0ceee814dcf84db9e2dae89321
Letenyei Ádám <letenyeidam89@gmail.com>
2020-11-21 18:07:52 +0100

    PdfToImage
    file split working, ascended writing not

--- src/main/service/pdfToImage/refactored/PdfToImage.java
+++ src/main/service/pdfToImage/refactored/PdfToImage.java
@@ -1,9 +1,11 @@
 package main.service.pdfToImage.refactored;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.ImageType;
 import org.apache.pdfbox.rendering.PDFRenderer;
 import org.apache.pdfbox.tools.imageio.ImageIOUtil;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -12,6 +14,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class PdfToImage {
+    static int pageCounter = 0;
 
     public static void convert(String sourceFilePath, String conversionDestinationPath, boolean split, int targetDpi, String targetFormat) {
         System.out.println("Convert method");
@@ -42,28 +45,48 @@ public class PdfToImage {
                 for (int i = 0; i < numOfPages; i++) {
                     System.out.println("Image writer method -> for");
                     try {
-                        specifiedImageWriter(tempPath, pdfRenderer.renderImage(i), i, targetDpi, targetFormat);
+                        specifiedImageWriter(tempPath, pdfRenderer.renderImageWithDPI(i,targetDpi, ImageType.RGB), i, targetDpi, targetFormat);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
             }
             synchronized (PdfToImage.class) {
-                for (int i = 0; i < numOfPages; i++) {
-
+                File[] wholeImages = new File(tempPath).listFiles();
+                for (int i = 0; i < wholeImages.length; i++) {
+                    try {
+                        System.out.println("Image writer method -> for -> split");
+                        //ImageIO.read(wholeImages[i]);
+                        BufferedImage[] images = imageCutter(ImageIO.read(wholeImages[i]));
+                        imagePartsWriter(conversionDestinationPath, images,targetDpi, targetFormat);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         } else {
             // Write the images to the destination folder
         }
     }
-    public synchronized static void specifiedImageWriter(String writeDestPath, BufferedImage image, int counter, int targetDpi, String targetFormat) {
+    public /*synchronized*/ static void specifiedImageWriter(String writeDestPath, BufferedImage image, int counter, int targetDpi, String targetFormat) {
         System.out.println("SpecifiedImageWriter method.");
         // image fileName dpi
         try {
             ImageIOUtil.writeImage(image, (writeDestPath + counter + "." + targetFormat), targetDpi);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+    public /*synchronized*/ static void imagePartsWriter(String destPath, BufferedImage[] images, int targetDpi, String targetFormat) {
+        for (int i = 0; i < images.length; i++) {
+            try {
+                synchronized (PdfToImage.class) {
+                    ImageIOUtil.writeImage(images[i], (destPath + pageCounter + "." + targetFormat), targetDpi);
+                }
+                ++pageCounter;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
     public static void createFolder(String conversionDestinationPath) {
@@ -79,5 +102,14 @@ public class PdfToImage {
         System.out.println("GetTempFolderPath method:");
         int slashIndex = conversionDestination.lastIndexOf("\\");
         return conversionDestination.substring(0, slashIndex).concat("\\temp\\");
+    }
+    public /*synchronized*/ static BufferedImage[] imageCutter(BufferedImage wholeImage) {
+        BufferedImage[] imageParts = new BufferedImage[2];
+        int width = wholeImage.getWidth();
+        int height = wholeImage.getHeight();
+        imageParts[0] = wholeImage.getSubimage(0, 0, width/2, height);
+        imageParts[1] = wholeImage.getSubimage(width/2,0, width/2, height);
+
+        return imageParts;
     }
 }
