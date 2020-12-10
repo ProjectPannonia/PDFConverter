2f837830f378a47d73ca4840e498d5459bc749dd
Letenyei Ádám <letenyeidam89@gmail.com>
2020-12-10 21:17:22 +0100

    Gui
    Updating gui style

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,28 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
+    private double xOffset = 0;
+    private double yOffset = 0;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("converter.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("fxmls/main.fxml"));
+        root.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+
+        root.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() - xOffset);
+            primaryStage.setY(event.getScreenY() - yOffset);
+        });
         primaryStage.setTitle("PDF converter");
-        primaryStage.setScene(new Scene(root, 1600, 900));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
