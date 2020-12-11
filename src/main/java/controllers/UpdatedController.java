154820de5d2f34a07cd3e54f97af53abbd06ed41
Letenyei Ádám <letenyeidam89@gmail.com>
2020-12-11 20:30:23 +0100

    Controller
    Implementing pdf to image controller methods

--- src/main/java/controllers/UpdatedController.java
+++ src/main/java/controllers/UpdatedController.java
@@ -1,21 +1,29 @@
 package controllers;
 
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import service.panes.pane1.DataForImageGeneration;
+import service.panes.pane1.PdfToImage;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class UpdatedController implements Initializable {
+
+    private ObservableList<String> fileFormats, targetDpi;
+    private FileChooser fileChooser;
+    private DirectoryChooser directoryChooser;
 
     @FXML
     private AnchorPane pnMain;
@@ -40,12 +48,12 @@ public class UpdatedController implements Initializable {
     /** buttons **/
     @FXML
     private Button btP1ChoosePDFPath, btP1ChooseDestinationFolder, btP1Convert;
-    /** text fields **/
+    /** Labels **/
     @FXML
-    private TextField tfP1PDFPath, tfP1DestinationPath;
+    private Label lbP1PDFPath, lbP1DestinationPath;
     /** choice boxes **/
     @FXML
-    private ChoiceBox<?> cbP1DestinationFormat, cbP1DestinationDPI;
+    private ChoiceBox<String> cbP1DestinationFormat, cbP1DestinationDPI;
     /** checkbox **/
     @FXML
     private CheckBox cbP1SplitImage;
@@ -58,10 +66,20 @@ public class UpdatedController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        fileFormats = FXCollections.observableArrayList("JPG","JPEG","PNG","TIF","TIFF","GIF","BMP");
+        cbP1DestinationFormat.setItems(fileFormats);
+        cbP1DestinationFormat.getStylesheets().add(getClass().getResource("../css/style.css").toExternalForm());
+        targetDpi = FXCollections.observableArrayList("100", "200", "300", "400", "500", "600");
+        cbP1DestinationDPI.setItems(targetDpi);
+        gridpanePdfToImage.setBackground(new Background(new BackgroundFill(Color.rgb(99, 69, 153), CornerRadii.EMPTY, Insets.EMPTY)));
+        gridpanePdfToImage.toFront();
+        btP1ChooseDestinationFolder.setDisable(true);
+        btP1Convert.setDisable(true);
+        cbP1DestinationFormat.setDisable(true);
+        cbP1DestinationDPI.setDisable(true);
     }
     @FXML
-    public void handleClicks(ActionEvent event) {
+    public void handleMainMenuClicks(ActionEvent event) {
         if(event.getSource() == btPdfToImage) {
             gridpanePdfToImage.setBackground(new Background(new BackgroundFill(Color.rgb(99, 69, 153), CornerRadii.EMPTY, Insets.EMPTY)));
             gridpanePdfToImage.toFront();
@@ -78,5 +96,41 @@ public class UpdatedController implements Initializable {
             Platform.exit();
         }
     }
-
+    /*** P1 -> PDF to images -> Button event handler ***/
+    @FXML
+    public void handleFirstPaneClicks(ActionEvent event) {
+        boolean sourceReady = false;
+        boolean destinationReady = false;
+        boolean destinationFormatReady = false;
+        boolean destinationDpiReady = false;
+        if(event.getSource() == btP1ChoosePDFPath) {
+            fileChooser = new FileChooser();
+            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PDF Files","*.pdf"));
+            File file = fileChooser.showOpenDialog(null);
+            System.out.println(file.getAbsolutePath());
+            if (file != null) {
+                lbP1PDFPath.setText(file.getAbsolutePath());
+                sourceReady = true;
+                file = null;
+                btP1ChooseDestinationFolder.setDisable(false);
+            }
+        } else if(event.getSource() == btP1ChooseDestinationFolder) {
+            directoryChooser = new DirectoryChooser();
+            File directory = directoryChooser.showDialog(null);
+            if (directory != null) {
+                lbP1DestinationPath.setText(directory.getAbsolutePath());
+                destinationReady = true;
+                destinationFormatReady = true;
+                destinationDpiReady = true;
+                cbP1DestinationDPI.setDisable(false);
+                cbP1DestinationFormat.setDisable(false);
+            }
+        } else if(event.getSource() == btP1Convert && sourceReady && destinationReady && destinationDpiReady && destinationFormatReady) {
+            btP1Convert.setDisable(false);
+            String pdfPath = lbP1PDFPath.getText();
+            String destinationPath = lbP1DestinationPath.getText();
+            DataForImageGeneration data = new DataForImageGeneration(pdfPath, destinationPath, cbP1SplitImage, destinationDpi, destinationFormat);
+            PdfToImage.convert(data);
+        }
+    }
 }
