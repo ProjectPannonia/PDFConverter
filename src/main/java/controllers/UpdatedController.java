2ac935659de33579383e6fc540765c0bfd704bbe
Letenyei Ádám <letenyeidam89@gmail.com>
2020-12-12 19:03:24 +0100

    Gui
    gui element names refactored

--- src/main/java/controllers/UpdatedController.java
+++ src/main/java/controllers/UpdatedController.java
@@ -14,6 +14,7 @@ import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import service.panes.pane1.DataForImageGeneration;
 import service.panes.pane1.PdfToImage;
+import service.panes.pane2.ImageSplitter;
 
 import java.io.File;
 import java.net.URL;
@@ -26,79 +27,118 @@ public class UpdatedController implements Initializable {
     private DirectoryChooser directoryChooser;
 
     @FXML
-    private AnchorPane pnMain;
+    private AnchorPane MainPane;
 
     @FXML
-    private VBox vbMenuBackground;
+    private VBox MainVerticalBox;
 
     @FXML
-    private Pane pnLogoBackground;
+    private Pane MainPaneLogo;
 
-    /*** Main-menu buttons ***/
+    /**
+     Main-menu buttons
+                    **/
     @FXML
-    private Button btPdfToImage, btImageSplitter, btMergeIntoPDF, btReadTextFromImages, btExit;
+    private Button  MainButtonPDFToImage,
+                    MainButtonImageSplitter,
+                    MainButtonMergeImages,
+                    MainButtonReadText,
+                    MainButtonExit;
 
-    /*** Content panes ***/
+    /**
+     Content panes
+                **/
     @FXML
-    private GridPane gridpanePdfToImage, gridpaneImageSplitter, gridpaneMergeImagesIntoPDF, gridpaneReadTextFromImages;
+    private GridPane    P1GridPanePDFToImage,
+                        P2GridPaneImageSlicer,
+                        P3GridPaneMergeImages,
+                        P4GridPaneReadText;
 
 
+    /**
+     P1 -> PDF to images
+                        **/
 
-    /*** P1 -> PDF to images ***/
-    /** buttons **/
+    /* Buttons */
     @FXML
-    private Button btP1ChoosePDFPath, btP1ChooseDestinationFolder, btP1Convert;
-    /** Labels **/
+    private Button  P1ButtonChoosePDF,
+                    P1ButtonChooseDestinationFolder,
+                    P1ButtonConvert;
+    /* Labels */
     @FXML
-    private Label lbP1PDFPath, lbP1DestinationPath;
-    /** choice boxes **/
+    private Label   P1LabelSourcePDFPath,
+                    P1LabelDestinationFolderPath;
+    /* Choice boxes */
     @FXML
-    private ChoiceBox<String> cbP1DestinationFormat, cbP1DestinationDPI;
-    /** checkbox **/
+    private ChoiceBox<String>   P1ChoiceBoxDestinationFormat,
+                                P1ChoiceBoxDestinationDPI;
+    /* Checkbox */
     @FXML
-    private CheckBox cbP1SplitImage;
+    private CheckBox P1CheckBoxSliceImage;
 
-    /*** P2 -> Image splitter ***/
-    /** Buttons **/
-    @FXML
-    private Button btP2ChooseImages, btP2ChooseDestinationFolder, btP2Split;
-    /** Labels **/
-    @FXML
-    private Label lbP2ImagesFolderPath, lbP2DestinationFolderPath;
 
-    /*** P3 -> Merge images into pdf ***/
+    /**
+     P2 -> Image slicer
+                        **/
+    /* Buttons */
+    @FXML
+    private Button  P2ButtonChooseImagesFolder,
+                    P2ButtonChooseDestinationFolder,
+                    P2ButtonSliceImages;
+    /* Labels */
+    @FXML
+    private Label   P2LabelImagesFolderPath,
+                    P2LabelDestinationFolderPath;
 
-    /*** P4 -> Read text from images ***/
+
+    /**
+     P3 -> Merge images into pdf
+                                **/
+    /* Buttons */
+    @FXML
+    private Button  P3ButtonChooseImagesFolder,
+                    P3ButtonChooseDestinationFolder,
+                    P3ButtonConvert;
+    /* Labels */
+    @FXML
+    private Label   P3LabelSourcePDFPath,
+                    P3LabelChooseImagesFolder;
+
+
+    /**
+     P4 -> Read text from images
+                                **/
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         fileFormats = FXCollections.observableArrayList("JPG","JPEG","PNG","TIF","TIFF","GIF","BMP");
-        cbP1DestinationFormat.setItems(fileFormats);
-        cbP1DestinationFormat.getStylesheets().add(getClass().getResource("../css/style.css").toExternalForm());
+        P1ChoiceBoxDestinationFormat.setItems(fileFormats);
+        P1ChoiceBoxDestinationFormat.getStylesheets().add(getClass().getResource("../css/style.css").toExternalForm());
         targetDpi = FXCollections.observableArrayList("100", "200", "300", "400", "500", "600");
-        cbP1DestinationDPI.setItems(targetDpi);
-        gridpanePdfToImage.setBackground(new Background(new BackgroundFill(Color.rgb(99, 69, 153), CornerRadii.EMPTY, Insets.EMPTY)));
-        gridpanePdfToImage.toFront();
-        btP1ChooseDestinationFolder.setDisable(true);
-        btP1Convert.setDisable(true);
-        cbP1DestinationFormat.setDisable(true);
-        cbP1DestinationDPI.setDisable(true);
+        P1ChoiceBoxDestinationDPI.setItems(targetDpi);
+        P1GridPanePDFToImage.setBackground(new Background(new BackgroundFill(Color.rgb(99, 69, 153), CornerRadii.EMPTY, Insets.EMPTY)));
+        P1GridPanePDFToImage.toFront();
+        P1ButtonChooseDestinationFolder.setDisable(true);
+        P1ButtonConvert.setDisable(true);
+        P1ChoiceBoxDestinationFormat.setDisable(true);
+        P1ChoiceBoxDestinationDPI.setDisable(true);
     }
     @FXML
     public void handleMainMenuClicks(ActionEvent event) {
-        if(event.getSource() == btPdfToImage) {
-            gridpanePdfToImage.setBackground(new Background(new BackgroundFill(Color.rgb(99, 69, 153), CornerRadii.EMPTY, Insets.EMPTY)));
-            gridpanePdfToImage.toFront();
-        } else if(event.getSource() == btImageSplitter) {
-            gridpaneImageSplitter.setBackground(new Background(new BackgroundFill(Color.rgb(99, 69, 153), CornerRadii.EMPTY, Insets.EMPTY)));
-            gridpaneImageSplitter.toFront();
-        } else if(event.getSource() == btMergeIntoPDF) {
-            gridpaneMergeImagesIntoPDF.setBackground(new Background(new BackgroundFill(Color.rgb(99, 69, 153), CornerRadii.EMPTY, Insets.EMPTY)));
-            gridpaneMergeImagesIntoPDF.toFront();
-        } else if(event.getSource() == btReadTextFromImages) {
-            gridpaneReadTextFromImages.setBackground(new Background(new BackgroundFill(Color.rgb(99, 69, 153), CornerRadii.EMPTY, Insets.EMPTY)));
-            gridpaneReadTextFromImages.toFront();
-        } else if(event.getSource() == btExit) {
+        if(event.getSource() == MainButtonPDFToImage) {
+            P1GridPanePDFToImage.setBackground(new Background(new BackgroundFill(Color.rgb(99, 69, 153), CornerRadii.EMPTY, Insets.EMPTY)));
+            P1GridPanePDFToImage.toFront();
+        } else if(event.getSource() == MainButtonImageSplitter) {
+            P2GridPaneImageSlicer.setBackground(new Background(new BackgroundFill(Color.rgb(99, 69, 153), CornerRadii.EMPTY, Insets.EMPTY)));
+            P2GridPaneImageSlicer.toFront();
+        } else if(event.getSource() == MainButtonMergeImages) {
+            P3GridPaneMergeImages.setBackground(new Background(new BackgroundFill(Color.rgb(99, 69, 153), CornerRadii.EMPTY, Insets.EMPTY)));
+            P3GridPaneMergeImages.toFront();
+        } else if(event.getSource() == MainButtonReadText) {
+            P4GridPaneReadText.setBackground(new Background(new BackgroundFill(Color.rgb(99, 69, 153), CornerRadii.EMPTY, Insets.EMPTY)));
+            P4GridPaneReadText.toFront();
+        } else if(event.getSource() == MainButtonExit) {
             Platform.exit();
         }
     }
@@ -109,36 +149,36 @@ public class UpdatedController implements Initializable {
         boolean destinationReady = false;
         boolean destinationFormatReady = false;
         boolean destinationDpiReady = false;
-        if(event.getSource() == btP1ChoosePDFPath) {
+        if(event.getSource() == P1ButtonChoosePDF) {
             fileChooser = new FileChooser();
             fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PDF Files","*.pdf"));
             File file = fileChooser.showOpenDialog(null);
 
             if (file != null) {
-                lbP1PDFPath.setText(file.getAbsolutePath());
+                P1LabelSourcePDFPath.setText(file.getAbsolutePath());
                 sourceReady = true;
                 file = null;
-                btP1ChooseDestinationFolder.setDisable(false);
+                P1ButtonChooseDestinationFolder.setDisable(false);
             }
-        } else if(event.getSource() == btP1ChooseDestinationFolder) {
+        } else if(event.getSource() == P1ButtonChooseDestinationFolder) {
             directoryChooser = new DirectoryChooser();
             File directory = directoryChooser.showDialog(null);
             if (directory != null) {
-                lbP1DestinationPath.setText(directory.getAbsolutePath());
+                P1LabelDestinationFolderPath.setText(directory.getAbsolutePath());
                 destinationReady = true;
                 destinationFormatReady = true;
                 destinationDpiReady = true;
-                cbP1DestinationDPI.setDisable(false);
-                cbP1DestinationFormat.setDisable(false);
-                btP1Convert.setDisable(false);
+                P1ChoiceBoxDestinationDPI.setDisable(false);
+                P1ChoiceBoxDestinationFormat.setDisable(false);
+                P1ButtonConvert.setDisable(false);
             }
-       } else if(event.getSource() == btP1Convert) {
-            btP1Convert.setDisable(false);
-            String pdfPath = lbP1PDFPath.getText();
-            String destinationPath = lbP1DestinationPath.getText() + "\\";
-            int destinationDpi = Integer.valueOf(cbP1DestinationDPI.getValue());
-            boolean splitWanted = cbP1SplitImage.isSelected();
-            String destinationFormat = cbP1DestinationFormat.getValue();
+       } else if(event.getSource() == P1ButtonConvert) {
+            P1ButtonConvert.setDisable(false);
+            String pdfPath = P1LabelSourcePDFPath.getText();
+            String destinationPath = P1LabelDestinationFolderPath.getText() + "\\";
+            int destinationDpi = Integer.valueOf(P1ChoiceBoxDestinationDPI.getValue());
+            boolean splitWanted = P1CheckBoxSliceImage.isSelected();
+            String destinationFormat = P1ChoiceBoxDestinationFormat.getValue();
 
 
             if(pdfPath != null && destinationPath != null && destinationFormat != null) {
@@ -150,17 +190,38 @@ public class UpdatedController implements Initializable {
     /*** P2 -> Image splitter -> Button event handler ***/
     @FXML
     public void handleSecondPaneClicks(ActionEvent event) {
-        if(event.getSource() == btP2ChooseImages) {
+        if(event.getSource() == P2ButtonChooseImagesFolder) {
             directoryChooser = new DirectoryChooser();
             File directory = directoryChooser.showDialog(null);
+            String pathToDirectory = null;
             if (directory != null) {
-                
+                pathToDirectory = directory.getAbsolutePath() + "\\";
+                P2LabelImagesFolderPath.setText(pathToDirectory);
             }
-        } else if(event.getSource() == btP2ChooseDestinationFolder) {
+        } else if(event.getSource() == P2ButtonChooseDestinationFolder) {
             directoryChooser = new DirectoryChooser();
             File directory = directoryChooser.showDialog(null);
-        } else if (event.getSource() == btP2Split) {
-
+            String pathToDirectory = null;
+            if (directory != null) {
+                pathToDirectory = directory.getAbsolutePath() + "\\";
+                P2LabelDestinationFolderPath.setText(pathToDirectory);
+            }
+        } else if (event.getSource() == P2ButtonSliceImages) {
+            String pathToFile = P2LabelImagesFolderPath.getText();
+            String conversionDestinationPath = P2LabelDestinationFolderPath.getText();
+            if (pathToFile != null) {
+                ImageSplitter.splitImage(pathToFile,conversionDestinationPath);
+            }
         }
+    }
+    /*** P3 -> Merge images into a pdf -> Button event handler ***/
+    @FXML
+    public void handleThirdPaneClicks(ActionEvent event) {
+
+    }
+    /*** P4 -> Read text from images -> Button event handler ***/
+    @FXML
+    public void handleFourthPaneClicks(ActionEvent event) {
+
     }
 }
